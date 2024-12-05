8a96af59631e74fa7a5b93b0d18891fb56eabc5d
Felipe Petrassi <petrassifelipe@gmail.com>
2024-12-05 08:51:29 -0300

    Delete Trabalho de PSC directory

--- Trabalho de PSC/MetaFit/Banco de Dados/Tabela Aluno_Treino.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Aluno_Treino (
-idAluno CHAR(36) NOT NULL,
-idTreino CHAR(36) NOT NULL,
-PRIMARY KEY (idAluno, idTreino),
-FOREIGN KEY (idAluno) REFERENCES Aluno(idAluno) ON DELETE CASCADE,
-FOREIGN KEY (idTreino) REFERENCES Treino(idTreino) ON DELETE CASCADE
-);
-select * from Aluno_Treino;
-drop table Aluno_Treino;
